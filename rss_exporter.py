04ff41e4fd4a66d41e400c54e374fe7d47d9f3c4
a.bajkulov-12 <baikulov.ant@gmail.com>
2023-12-28 19:25:27 +0300

    test rss

--- rss_exporter.py
+++ rss_exporter.py
@@ -11,6 +11,8 @@ from telegram.constants import ParseMode
 from telegram.error import TelegramError
 from html import unescape
 from bs4 import BeautifulSoup
+import requests
+from urllib.parse import urljoin
 
 async def send_to_telegram(title, description, link):
     bot = Bot(token=BOT_TOKEN)
@@ -19,11 +21,11 @@ async def send_to_telegram(title, description, link):
     soup = BeautifulSoup(description, 'html.parser')
     cleaned_description = soup.get_text()
 
-    # Извлекаем URL изображения (если есть)
-    image_url = None
+    # Извлекаем URL изображения из тега <img>
     image_tag = soup.find('img')
+    image_url = None
     if image_tag:
-        image_url = image_tag.get('src')
+        image_url = urljoin('https://baikulov.github.io/wiki/blog/', image_tag.get('src'))
 
     # Декодирование HTML-сущностей
     cleaned_description = unescape(cleaned_description)
@@ -36,8 +38,14 @@ async def send_to_telegram(title, description, link):
     message = f"\n{cleaned_description}\n\n[Читать далее]({link})"
 
     try:
-        await bot.send_message(chat_id=CHANNEL_ID, text=message, parse_mode=ParseMode.MARKDOWN)
-        print(f"Сообщение успешно отправлено: {title}")
+        # Загружаем изображение и отправляем его вместе с сообщением
+        if image_url:
+            print(f"Изображение успешно скачано: {image_url}")
+            image_data = requests.get(image_url).content
+            await bot.send_photo(chat_id=CHANNEL_ID, photo=image_data, caption=message, parse_mode=ParseMode.MARKDOWN)
+        else:
+            print("Изображение не найдено.")
+            await bot.send_message(chat_id=CHANNEL_ID, text=message, parse_mode=ParseMode.MARKDOWN)
     except TelegramError as e:
         print(f"Ошибка при отправке сообщения: {e}")
 
@@ -55,5 +63,4 @@ async def main():
     rss_feed_url = RSS_URL
     await check_rss_feed(rss_feed_url)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
